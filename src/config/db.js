d319343a67c768c9f47bb099ae53c51d3a70cb39
shishiro <bannu1302@gmail.com>
2025-01-11 16:23:58 +0530

    updated log

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    log: [{
+        emit: 'event',
+        level: 'query',
+    }]
+});
+
+prisma.$on('query', (e) => {
+    console.log('Query: ' + e.query);
+});
 
 export default prisma;
